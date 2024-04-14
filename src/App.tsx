9299bb749a93faa6ca55367742696eaffe00af16
vikram-hegde <vikramhegde1440@gmail.com>
2024-04-15 01:32:28 +0530

    ✅ Overview section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import AddIcon from '~icons/solar/add-circle-linear'
 import BellIcon from '~icons/solar/bell-linear'
 import ChatIcon from '~icons/solar/chat-line-linear'
 import ChevronDown from '~icons/solar/alt-arrow-down-outline'
+import BagIcon from '~icons/solar/bag-2-linear'
+import GraphIcon from '~icons/solar/graph-new-outline'
 
 function App() {
 	return (
@@ -49,7 +51,7 @@ function App() {
 			<main className="px-9 mt-3">
 				<h1 className="font-semibold text-4xl">Dashboard</h1>
 				<div className="grid">
-					<section className="overview p-4">
+					<section className="overview p-6 mt-8 rounded-lg">
 						<div className="flex justify-between">
 							<h2 className="font-semibold text-2xl">Overview</h2>
 							<Button
@@ -60,6 +62,48 @@ function App() {
 								endIcon={<ChevronDown />}
 							>
 								All time
+							</Button>
+						</div>
+						<div className="bg-gray-100 flex p-2 mt-6 rounded-lg">
+							<div className="flex gap-3 bg-gray-50 px-4 py-6 flex-1 rounded-md shadow-md">
+								<span className="bg-teal-300 text-teal-900 self-start p-2 rounded-full">
+									<BagIcon className="h-6 w-6" />
+								</span>
+								<div>
+									<h3 className="font-semibold text-gray-500 text-xl">
+										Customers
+									</h3>
+									<p className="text-gray-900 tabular-nums text-6xl font-semibold">
+										2048
+									</p>
+								</div>
+							</div>
+							<div className="flex gap-3 px-4 py-6 flex-1">
+								<span className="bg-violet-300 text-violet-900 self-start p-2 rounded-full">
+									<GraphIcon className="h-6 w-6" />
+								</span>
+								<div>
+									<h3 className="font-semibold text-gray-500 text-xl">
+										Income
+									</h3>
+									<p className="text-gray-900 tabular-nums text-6xl font-semibold">
+										250K
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="mt-4 flex items-start justify-between">
+							<span className="max-w-60">
+								Welcome <strong>302 customers</strong> with a nice personal
+								message
+							</span>
+							<Button
+								className="font-bold py-2"
+								variant="secondary"
+								borderAccent="secondary"
+								endIcon={<ChatIcon className="h-4 w-4" />}
+							>
+								Send Message
 							</Button>
 						</div>
 					</section>
